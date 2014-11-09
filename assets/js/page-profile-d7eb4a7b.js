27127af07dbaeb4e267221487d0cd7d6d8cd3f36
wansaleh <wansaleh@gmail.com>
2014-11-09 19:26:42 +0800

    Optimize.

--- assets/js/page-profile-d7eb4a7b.js
+++
@@ -1,1 +0,0 @@
-(function(){$(function(){return $(".lang-btn-en").click(function(){return $(".profile-outer").attr("lang","en")}),$(".lang-btn-ms").click(function(){return $(".profile-outer").attr("lang","ms")}),$("article h1, article h2, article p").widowFix()})}).call(this);
